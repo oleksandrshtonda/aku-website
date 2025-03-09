0f47529830c14f672c3efa9c282c2222686c5b24
Oleksandr <oleksandrshtonda@gmail.com>
2025-03-09 13:18:38 +0100

    feat: new burger menu for mobile

--- frontend/src/commonComponents/Header/BurgerMenu/BurgerMenuLink.tsx
+++ frontend/src/commonComponents/Header/BurgerMenu/BurgerMenuLink.tsx
@@ -1,7 +1,7 @@
 import { FC, useState } from 'react';
 import { ILink } from '../../interfaces/ILink.ts';
-import { NavLink } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import classNames from 'classnames';
 
 interface Props extends ILink {
   subLinks: ILink[];
@@ -19,19 +19,12 @@ const BurgerMenuLink: FC<Props> = ({
     <li
       className="burger-nav__item item"
     >
-      <NavLink
-        to={href}
+      <a
+        href={href}
         className="item__link"
       >
         {t(label)}
-      </NavLink>
-      
-      {subLinks.length > 0 && (
-        <button
-          onClick={() => setToggled(v => !v)}
-          className="item__btn"
-        />
-      )}
+      </a>
       
       {toggled && subLinks.length > 0 && (
         <ul className="item__sublinks">
@@ -43,6 +36,13 @@ const BurgerMenuLink: FC<Props> = ({
             </li>
           ))}
         </ul>
+      )}
+      
+      {subLinks.length > 0 && (
+        <button
+          onClick={() => setToggled(v => !v)}
+          className={classNames('item__btn', toggled ? 'item__btn--up-arrow' : 'item__btn--down-arrow')}
+        />
       )}
     </li>
   );
